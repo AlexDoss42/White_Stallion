3bbe9cda477136d127a407448587068e58a47e21
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-10 18:13:47 -0600

    Set up backend Crud

--- server/controllers/ServiceRecords.js
+++ server/controllers/ServiceRecords.js
@@ -5,40 +5,57 @@ let serviceLog = [
     price: '$50.00',
     milesDriven: 520000,
     partName: 'BrakePads',
-    Date: '5/5/18'
+    dateOfService: '5/5/18'
   },
   {
     id: id++,
     price: '$500.00',
     milesDriven: 55000,
     partName: 'AC Compressor',
-    Date: '7/3/18'
+    dateOfService: '7/3/18'
   },
   {
     id: id++,
     price: '$150.00',
     milesDriven: 86000,
     partName: 'Radiator',
-    Date: '11/25/18'
+    dateOfService: '12/25/18'
   }
 ]
 
 module.exports = {
-  read: (req, res) => res.send(serviceLog),
-
+  
   create: (req, res) => {
-
-    res.send(serviceLog)
-  },
-
+    
+    let newServiceRecord = req.body
+    newServiceRecord.id = id++
+      
+      serviceLog.push(newServiceRecord)
+      res.send(serviceLog)
+    },
+    
+  read: (req, res) => res.send(serviceLog),
+  
   update: (req, res) => {
 
+    let { id } = req.params
+    let updatedServiceRecord = req.body
+    updatedServiceRecord.id = id
+
+    let index = serviceLog.findIndex(updatedServiceRecord => +updatedServiceRecord.id === +id)
+
+    serviceLog.splice(index, 1, updatedServiceRecord)
     res.send(serviceLog)
   },
 
   delete: (req, res) => {
+    let { id } = req.params
+    let deletedServiceRecord = req.body
+    deletedServiceRecord.id = id
 
+    let index = serviceLog.findIndex(deletedServiceRecord => +deletedServiceRecord.id === +id)
+
+    serviceLog.splice(index, 1)
     res.send(serviceLog)
   }
-
 }
--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/service', ServiceCtrl.read)
-app.post('/service', ServiceCtrl.create)
-app.put('/service/:id', ServiceCtrl.update)
-app.delete('/service/:id', ServiceCtrl.delete)
+app.get('/api/service', ServiceCtrl.read)
+app.post('/api/service', ServiceCtrl.create)
+app.put('/api/service/:id', ServiceCtrl.update)
+app.delete('/api/service/:id', ServiceCtrl.delete)
 
 const port = 4242
 app.listen(port, () => console.log(`It's over Anakin! I have the ${port} port`))
