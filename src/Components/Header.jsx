6e1884e67c2c2854cf08aca258cbff743af618c4
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-11 15:39:01 -0600

    Add Background color and beautifies header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,19 @@ class Header extends Component {
   render() {
 
     return (
-      <div>
-        <header>
-          <h1>El SEMENTAL BLANCO</h1>
-        </header>
+      <div className='headerComponent'>
+        <div className='logo'>
+          <i class="fas fa-car-side fa-2x"></i>
+        </div>
+        <div>
+          <header>
+            <h1>El SEMENTAL BLANCO</h1>
+          </header>
+        </div>
+        <div className='navIcons'>
+          <i class="fas fa-plus fa-2x"></i>
+          <i class="fas fa-search fa-2x"></i>
+        </div>
       </div>
     )
   } 
