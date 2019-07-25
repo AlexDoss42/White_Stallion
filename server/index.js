58987a04abca7a92a9052a4d85a247417a87330a
Alex Doss <alex.doss42@gmail.com>
2019-07-24 23:29:09 -0600

    NPM installs sessions, sets up massive and sessions in server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
+require('dotenv').config()
 const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
 
 const ServiceCtrl = require('./controllers/ServiceRecords')
 
 const app = express()
 
+const { SESSION_SECRET, CONNECTION_STRING } = process.env
 
 app.use(express.json())
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 365
+  }
+}))
+
+massive(CONNECTION_STRING)
+  .then((db) => {
+    app.set('db', db)
+    console.log(`It's working! IT'S WORKING!!!`)
+    console.log(db.listTables())
+  })
 
 app.get('/api/service', ServiceCtrl.read)
 app.post('/api/service', ServiceCtrl.create)
