08eb5972421520bcd4c64fa90a0fc89f29ed9b96
AlexDoss42 <alex.doss42@gmail.com>
2022-05-03 23:55:54 -0600

    cleans up a console.log

--- src/Pages/Garage.jsx
+++ src/Pages/Garage.jsx
@@ -1,16 +1,15 @@
-import Axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Footer from '../Components/Footer'
-import Header from '../Components/Header'
-import Vehicle from '../Components/Vehicle'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
+import Vehicle from '../Components/Vehicle';
 
 function Garage() {
 
     const [vehicles, setVehicles] = useState([])
 
     useEffect(() => {
-        Axios.get('/api/vehicle/1').then(res => { 
-            console.log(123, res);
+        axios.get('/api/vehicle/1').then(res => { 
           setVehicles(res.data);
         })
         .catch(err => console.log('Houston we have a problem: ', err))
@@ -20,7 +19,7 @@ function Garage() {
         <Header />
         {vehicles.map(vehicle => {
           return <Vehicle
-                    // key={vehicle.vehicle_id}
+                    key={vehicle.vehicle_id}
                     vehicleRecord={vehicle}
                 />
         })} 
