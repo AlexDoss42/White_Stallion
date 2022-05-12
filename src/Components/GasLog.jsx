fbd64374bac8c9ef13a8c22db26c740b95a8c731
AlexDoss42 <alex.doss42@gmail.com>
2022-05-11 23:32:12 -0600

    builds out a skeleton of gaslog

--- src/Components/GasLog.jsx
+++ src/Components/GasLog.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 
 function GasLog() {
-  return (
-    <div>GasLog</div>
-  )
+  const [fillUpLogs, setFillUpLogs] = useState([])
+
+// Displays the complete Service log upon Mounting
+
+useEffect(() => {
+  axios.get(`/api/fill_up/${vehicle_id}`).then(res => {
+    setFillUpLogs(res.data);
+  })
+  .catch(err => console.log('Houston we have a problem: ', err))
+}, [])
+
+  const updateFillUpRecord = (fillUpRecord) => {
+    axios.put(`/api/fill_up/${fillUpRecord.id}`, fillUpRecord).then(res => {
+      setFillUpLogs(res.data);
+    })
+    .catch(err => console.log('Houston, we have a problem: ', err))
+  }
+
+  const deleteFillUpRecord = (fillUpRecord) => {
+    axios.delete(`/api/fill_up/${fillUpRecord.id}`, fillUpRecord)
+    .then(res => { 
+      setServiceLogs(res.data);
+    })
+    .catch(err => console.log('Houston, we have a problem: ', err))
+  }
+
+    return (
+
+      <div className='ServiceLog'>
+      {fillUpLogs.map(fillUpRecord => {
+          return <GasFillRecord
+                  key = { fillUpRecord.id }
+                  serviceRecord = { fillUpRecord }
+                  updateFillUpRecord = { updateFillUpRecord }
+                  deleteFillUpRecord = { deleteFillUpRecord }/>
+        })}   
+      </div>
+      
+    )
+}
 }
 
 export default GasLog;
