e93f6a07f2673bcbed8b7f8ea828c9d6d391eb9a
AlexDoss42 <alex.doss42@gmail.com>
2022-05-14 22:44:03 -0600

    sets up firebase connection

---
+++ server/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC07TT-hv-Cak1jdapVqa3W0P_ErBdYgP4",
+  authDomain: "white-stallion-c1e7c.firebaseapp.com",
+  projectId: "white-stallion-c1e7c",
+  storageBucket: "white-stallion-c1e7c.appspot.com",
+  messagingSenderId: "546871590564",
+  appId: "1:546871590564:web:520016f74fb5bcd5c8f031"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
